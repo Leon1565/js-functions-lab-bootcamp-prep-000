51685c2099672a3e8f66fe704fd9750074c9151e
Cha Yang <chachaiyang3@gmail.com>
2019-07-18 04:14:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // write your code below!
+function happyHolidays()
+{
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name)
+{
+  return `Happy holidays, ${name}!`
+}
+happyHolidaysTo('Cha')
+
+function happyHolidayTo(holiday, name)
+{
+  return `Happy ${holiday}, ${name}!`
+}
+happyHolidayTo('Christmas', 'Cha')
+
+function holidayCountdown(days, holiday)
+{
+  return `It's ${days} days until ${holiday}!`
+}
+holidayCountdown(20, 'Mother\'s Day')
